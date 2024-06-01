4564de0ca5974c6252819aebbe4ed34a14fefe28
serjik__1 <95130963+govnar4ick@users.noreply.github.com>
2024-06-01 19:44:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0;
+            Console.WriteLine("введите x");
+            x = Convert.ToInt32(Console.ReadLine());
+            double a = 4*Math.Sin(x)*5*Math.Sin(x);
+            Console.WriteLine("функция равна =" + a);
+            Console.ReadLine();
+        }
+    }
+}
